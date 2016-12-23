edebdb6fc4c954cc933e741c062594c3c1346353
pkatira7 <pkatira@vmware.com>
2016-12-23 15:05:16 +0530

    Add files via upload

---
+++ Expressions.java
@@ -0,0 +1,20 @@
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	int a = 20,b = 10;
+	 b= a-- - --a;
+	 System.out.println(b);
+	int c=a--;
+	System.out.println(c);
+	int d=a>>2;
+	System.out.println(d);
+	int e=a&b;
+	System.out.println(e);
+
+}
+}
